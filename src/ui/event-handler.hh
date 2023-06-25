ca194e0a20d18b2adce631c58af487fa8c78bdf3
Downvot_ED <leroistreaming@gmail.com>
2023-06-25 23:16:52 +0200

    chore(event-handler): added documentation

--- src/ui/event-handler.hh
+++ src/ui/event-handler.hh
@@ -2,14 +2,36 @@
 #include <glfw/glfw3.h>
 #include "camera/camera.hh"
 
+
+/**
+ * @brief EventHandler class is responsible for handling user input.
+ */
 class EventHandler
 {
 public:
     EventHandler(GLFWwindow* window, Camera& camera);
 
+    /**
+     *  @brief Registers callbacks for user input.
+     */
     void registerCallbacks();
+    /**
+     * @brief Right and left mouse click callback.
+     * @param window window object.
+     * @param xpos x position.
+     * @param ypos y position.
+     */
     static void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+    /**
+     * @brief mouse scroll callback for zooming in and out.
+     * @param window window object.
+     * @param xoffset x offset.
+     * @param yoffset y offset.
+     */
     static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+    /**
+     *  @brief gets the camera object.
+     */
     Camera& getCamera() const;
 
 private:
